0264ac67049b7ef00d0005c5296dd80f2d1e2252
tpubz <taylor.pubins@icloud.com>
2020-09-27 20:49:39 -0600

    Initial Commit

---
+++ WishLisht/WishLishtApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishLishtApp.swift
+//  WishLisht
+//
+//  Created by Taylor Pubins on 9/27/20.
+//
+
+import SwiftUI
+
+@main
+struct WishLishtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
